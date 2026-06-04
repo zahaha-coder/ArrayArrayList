6191ef496d02d2620dadca9408a55527866cb6c2
zahaha-coder <rezahaha2134@gmail.com>
2026-06-04 22:15:39 +0800

    Update MainPerpustakaan.java

--- src/bagian3/perpustakaan/MainPerpustakaan.java
+++ src/bagian3/perpustakaan/MainPerpustakaan.java
@@ -2,20 +2,30 @@ package bagian3.perpustakaan;
 
 public class MainPerpustakaan {
     public static void main(String[] args) {
-        // Membuat objek pengelola
+        
         Perpustakaan perpus = new Perpustakaan();
-        // Membuat objek Buku lalu memasukkannya ke koleksi
-        perpus.tambahBuku(new Buku("Laskar Pelangi", "Andrea Hirata"));
-        perpus.tambahBuku(new Buku("Bumi Manusia", "Pramoedya"));
-        perpus.tambahBuku(new Buku("Negeri 5 Menara", "Ahmad Fuadi"));
-        perpus.tampilkanKoleksi();
+
+        // Membuat objek Buku 
+        perpus.tambahBuku(new Buku("Laskar Pelangi", "Andrea Hirata", 2005));
+        perpus.tambahBuku(new Buku("Bumi Manusia", "Pramoedya", 1980));
+        perpus.tambahBuku(new Buku("Negeri 5 Menara", "Ahmad Fuadi", 2009));
         
+        perpus.tampilkanKoleksi();
         System.out.println();
+        
+        //pinjam buku
         perpus.pinjamBuku("Bumi Manusia");
-        perpus.pinjamBuku("Bumi Manusia"); // coba pinjam kedua kali
-        
-        System.out.println();
-        perpus.tampilkanKoleksi();
         System.out.println("Buku tersedia: " + perpus.jumlahTersedia());
+        System.out.println();
+        
+        //Mengembalikan buku yang sedang dipinjam
+        System.out.println("--- Menguji Fitur Pengembalian Buku ---");
+        perpus.kembalikanBuku("Bumi Manusia");
+        System.out.println("Buku tersedia: " + perpus.jumlahTersedia());
+        System.out.println();
+        
+        //Mencari buku berdasarkan nama penulis
+        System.out.println("--- Menguji Fitur Pencarian Penulis ---");
+        perpus.cariPenulis("Andrea Hirata");
     }
 }
